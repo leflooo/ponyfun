a5c880f67850aa01f07df650693352ffa2fa8fe2
leflo <le.flo@icloud.com>
2021-02-25 14:34:42 +0100

    #3 cbook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.scss';
+import PonyList from './PonyList';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const conf = {
+    Url: 'http://localhost:3000/contacts'
+  };
+
+ReactDOM.render(<PonyList config={conf} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
