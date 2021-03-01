ea2a4bb23b874ebd57bb1ca4e5e6b853e92ed91b
leflo <le.flo@icloud.com>
2021-03-01 10:22:35 +0100

    #3 cbook

--- src/PonyList.js
+++ src/PonyList.js
@@ -82,7 +82,7 @@ class PonyList extends React.Component {
   render() {
     return <div>
       <header>
-        <h1>Kontaktbuch</h1>
+        <h1>Kontaktbuch!!!</h1>
       </header>
 
         <main>
